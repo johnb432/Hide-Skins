c2299041d3441318e3719843ffa0c0c788402028
johnb432 <58661205+johnb432@users.noreply.github.com>
2022-12-09 19:03:48 +0100

    1.0.0.1

--- mod.cpp
+++ mod.cpp
@@ -1,15 +1,15 @@
+// https://community.bistudio.com/wiki/modParams
 name = "Hide Skins";
 author = "johnb43";
-tooltip = "Hide Skins";
-tooltipOwned = "Hide Skins";
-hideName = 0;
-hidePicture = 0;
-actionName = "Github";
-action = "https://github.com/johnb432/Hide-Skins";
-description = "A mod that replaces some special models with regular variants of that model.";
-overview = "A mod that replaces some special models with regular variants of that model.";
+// https://community.bistudio.com/wiki/ImageToPAA
 picture = "\z\hide_skins\addons\main\ui\logo_hide_skins.paa";
 logo = "\z\hide_skins\addons\main\ui\logo_hide_skins.paa";
 logoOver = "\z\hide_skins\addons\main\ui\logo_hide_skins.paa";
 logoSmall = "\z\hide_skins\addons\main\ui\logo_hide_skins.paa";
-overviewPicture = "\z\hide_skins\addons\main\ui\logo_hide_skins.paa";
+tooltip = "Hide Skins";
+tooltipOwned = "Hide Skins";
+action = "https://github.com/johnb432/Hide-Skins";
+actionName = "Github";
+overview = "A mod that replaces some special models with regular variants of that model.";
+hidePicture = 0;
+hideName = 0;
